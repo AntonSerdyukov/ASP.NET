1dab908f01d0758859aad89a409f8a379df00548
AntonSerdyukov <743-34-87@mail.ru>
2019-10-17 21:08:53 +0300

    Task 3 of Module 2 was completed.

---
+++ Module2_3/Module2_3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {           
+            Console.WriteLine("Введите первое число");
+            bool firstCheck = double.TryParse(Console.ReadLine().Replace(".",","),out double firstNum);
+            Console.WriteLine("Введите второе число");           
+            bool secondCheck = double.TryParse(Console.ReadLine().Replace(".",","),out double secondNum);
+            if (firstCheck==true && secondCheck == true)
+            {
+                double temp = secondNum;
+                secondNum = firstNum;
+                firstNum=temp;
+                Console.WriteLine("Значение первого числа:{0}",firstNum);
+                Console.WriteLine("Значение второго числа:{0}", secondNum);
+            }
+            else
+            {               
+                Console.WriteLine("Неправильно введены данные");
+            }
+            Console.ReadLine();
+        }
+    }
+}
