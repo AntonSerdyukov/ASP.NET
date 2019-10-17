228418867ef8f3c35e6d330d54f88666285b3007
AntonSerdyukov <743-34-87@mail.ru>
2019-10-17 20:56:37 +0300

    New project was created.Task 1 of Module 2 was completed.

---
+++ Module2_1/Module2_1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество компаний в стране");
+            int companiesAmount = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите процент налога");
+            double percentTax = double.Parse(Console.ReadLine());
+            double companyProfit = 500;
+            double sumTax = companyProfit * percentTax/100 * companiesAmount;
+            Console.WriteLine("Суммарный налог:"+sumTax);
+            Console.ReadLine();
+        }
+    }
+}
