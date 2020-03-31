4395d7320e2d1d4fa0aea45637aca2fff29ed840
AntonSerdyukov <743-34-87@mail.ru>
2020-03-31 23:44:26 +0300

    Adding search procedures and others.

--- ElectroCardsWebApplicationVariant7/ElectroCardsWebApplicationVariant7/Controllers/UserController.cs
+++ ElectroCardsWebApplicationVariant7/ElectroCardsWebApplicationVariant7/Controllers/UserController.cs
@@ -3,15 +3,87 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ElectroCardsWebApplicationVariant7.Models;
 
 namespace ElectroCardsWebApplicationVariant7.Controllers
 {
     public class UserController : Controller
     {
-        // GET: User
+       
+        ApplicationCardsDbEntities2 db = new ApplicationCardsDbEntities2();
+
         public ActionResult Index()
         {
             return View();
         }
+
+        public ActionResult DisplayPatients()
+        {
+            var patients = db.PatientDatas;
+            return View(patients.ToList());
+        }
+
+        [HttpGet]
+        public ActionResult AddPatient(int id)
+        {
+
+            PatientData patient = db.PatientDatas.Find(id);
+
+            db.PatientDatas.Remove(patient);
+            db.SaveChanges();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddPatient(PatientData patient)
+        {
+            db.PatientDatas.Add(patient);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        
+
+        
+        public ActionResult SearchForPatientsByRecordingDate(string searchString)
+        {
+            var patients = from p in db.PatientDatas
+                           select p ;
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                patients = patients.Where(p => p.RecordDate.ToString().Contains(searchString));
+            }
+      
+            return View(patients.ToList());
+        }
+
+        public ActionResult SearchForPatientsByDiagnose(string searchString)
+        {
+            var patients = from p in db.PatientDatas
+                           select p;
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                patients = patients.Where(p => p.Diagnose.Contains(searchString));
+            }
+
+            return View(patients.ToList());
+        }
+
+        public ActionResult SearchForDoctorsByPosition(string searchString)
+        {
+            var doctors = from d in db.Doctors
+                           select d;
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                doctors = doctors.Where(d => d.Position.Contains(searchString));
+            }
+
+            return View(doctors.ToList());
+        }
+
+        
+
+
+
     }
 }
